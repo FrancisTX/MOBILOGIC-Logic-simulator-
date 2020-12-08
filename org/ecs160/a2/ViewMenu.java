cd87d372e33e1543a9383263059ad63803157073
Zamikho <subecerra@ucdavis.edu>
2020-12-08 15:08:22 -0800

    ignore
    dsaf

--- org/ecs160/a2/ViewMenu.java
+++ org/ecs160/a2/ViewMenu.java
@@ -3,6 +3,10 @@ import com.codename1.ui.*;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import org.ecs160.a2.Objects.Gate.GateAND;
+<<<<<<< HEAD
+=======
+import com.codename1.ui.Command;
+>>>>>>> parent of e3156b4... Merge branch 'master' into Sam
 import org.ecs160.a2.Utilities.WorkspaceUtil;
 import org.ecs160.a2.UI.TrashCan;
 
@@ -20,7 +24,24 @@ public class ViewMenu extends Container {
         });
         this.add(testingSaveButton);
 
+<<<<<<< HEAD
         this.add(new TrashCan(workspace));
+=======
+        Image canImg;
+        String url = System.getProperty("user.dir") + "/src/Images/trash_can.jpg";
+        try {
+            canImg = Image.createImage("file:" + url);
+            Button Trash = new Button(canImg);
+            //now add a click listener that will remove the selected widget
+            Trash.addActionListener(evt -> {
+                workspace.removeHighlighted();
+                workspace.repaint();
+            });
+            this.add(Trash);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+>>>>>>> parent of e3156b4... Merge branch 'master' into Sam
 
         Button addANDButton = new Button("And Gate");
         addANDButton.addActionListener((evt) -> {
