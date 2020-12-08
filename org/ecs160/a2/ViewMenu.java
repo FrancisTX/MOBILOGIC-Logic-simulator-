53376f27a60e683d1f3eb0e7c5714ea663242a5f
Zamikho <subecerra@ucdavis.edu>
2020-12-08 14:03:17 -0800

    Revert "Load Subcircuit Added to taskbar"
    This reverts commit 8ed356e2215bfbea480f276b5278155ba3457183.

--- org/ecs160/a2/ViewMenu.java
+++ org/ecs160/a2/ViewMenu.java
@@ -3,10 +3,8 @@ import com.codename1.ui.*;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import org.ecs160.a2.Objects.Gate.GateAND;
-import com.codename1.ui.Command;
 import org.ecs160.a2.Utilities.WorkspaceUtil;
-
-import java.io.IOException;
+import org.ecs160.a2.UI.TrashCan;
 
 public class ViewMenu extends Container {
     private ViewWorkspace workspace;
@@ -18,28 +16,11 @@ public class ViewMenu extends Container {
         this.setLayout(new BoxLayout(BoxLayout.X_AXIS));
         Button testingSaveButton = new Button("Save");
         testingSaveButton.addActionListener((evt) -> {
-            TextField circuitName = new TextField("", "Circuit Name", 20, TextArea.ANY);
-            Command enter = new Command("Ok");
-            if(Dialog.show("Save the Circuit", circuitName, enter, new Command("Cancel")) == enter){
-                workspace.mainCircuit.save(circuitName.getText());
-            }
+            workspace.mainCircuit.save("Testing");
         });
         this.add(testingSaveButton);
 
-        Image canImg;
-        String url = System.getProperty("user.dir") + "/src/Images/trash_can.jpg";
-        try {
-            canImg = Image.createImage("file:" + url);
-            Button Trash = new Button(canImg);
-            //now add a click listener that will remove the selected widget
-            Trash.addActionListener(evt -> {
-                workspace.removeHighlighted();
-                workspace.repaint();
-            });
-            this.add(Trash);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this.add(new TrashCan(workspace));
 
         Button addANDButton = new Button("And Gate");
         addANDButton.addActionListener((evt) -> {
