8ed356e2215bfbea480f276b5278155ba3457183
Zamikho <subecerra@ucdavis.edu>
2020-12-07 23:42:05 -0800

    Load Subcircuit Added to taskbar
    -Changes also include showing a list of saved circuits

--- org/ecs160/a2/ViewMenu.java
+++ org/ecs160/a2/ViewMenu.java
@@ -3,8 +3,10 @@ import com.codename1.ui.*;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import org.ecs160.a2.Objects.Gate.GateAND;
+import com.codename1.ui.Command;
 import org.ecs160.a2.Utilities.WorkspaceUtil;
-import org.ecs160.a2.UI.TrashCan;
+
+import java.io.IOException;
 
 public class ViewMenu extends Container {
     private ViewWorkspace workspace;
@@ -16,11 +18,28 @@ public class ViewMenu extends Container {
         this.setLayout(new BoxLayout(BoxLayout.X_AXIS));
         Button testingSaveButton = new Button("Save");
         testingSaveButton.addActionListener((evt) -> {
-            workspace.mainCircuit.save("Testing");
+            TextField circuitName = new TextField("", "Circuit Name", 20, TextArea.ANY);
+            Command enter = new Command("Ok");
+            if(Dialog.show("Save the Circuit", circuitName, enter, new Command("Cancel")) == enter){
+                workspace.mainCircuit.save(circuitName.getText());
+            }
         });
         this.add(testingSaveButton);
 
-        this.add(new TrashCan(workspace));
+        Image canImg;
+        String url = System.getProperty("user.dir") + "/src/Images/trash_can.jpg";
+        try {
+            canImg = Image.createImage("file:" + url);
+            Button Trash = new Button(canImg);
+            //now add a click listener that will remove the selected widget
+            Trash.addActionListener(evt -> {
+                workspace.removeHighlighted();
+                workspace.repaint();
+            });
+            this.add(Trash);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         Button addANDButton = new Button("And Gate");
         addANDButton.addActionListener((evt) -> {
