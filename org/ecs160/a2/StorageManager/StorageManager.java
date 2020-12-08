ab4d3bf1972158b15072a09ba5401bf6f5ed21ca
Zamikho <subecerra@ucdavis.edu>
2020-12-08 15:09:33 -0800

    ignore

--- org/ecs160/a2/StorageManager/StorageManager.java
+++ org/ecs160/a2/StorageManager/StorageManager.java
@@ -18,4 +18,40 @@ public class StorageManager {
         if (savable == null) return null;
         return savable.extractCircuit();
     }
+<<<<<<< HEAD
+=======
+
+    public Circuit loadMain(String circuitName) {
+        CircuitSavable savable = (CircuitSavable)Storage.getInstance().readObject(circuitName);
+        if (savable == null) return null;
+        Circuit mainCircuit = savable.extractCircuit();
+        HashMap<String, ArrayList<ArrayList<Integer>>> coordinates = savable.coordinates;
+
+        ArrayList<ArrayList<Integer>> circuitPosPairs = coordinates.get("Circuit");
+        for (int i = 0; i < circuitPosPairs.size(); i++) {
+            ArrayList<Integer> pair = circuitPosPairs.get(i);
+            mainCircuit.getSubCircuits().get(i).setCoordinates(pair.get(0), pair.get(1));
+        }
+
+        ArrayList<ArrayList<Integer>> switchPosPairs = coordinates.get("Switch");
+        for (int i = 0; i < switchPosPairs.size(); i++) {
+            ArrayList<Integer> pair = switchPosPairs.get(i);
+            mainCircuit.getSwitches().get(i).setCoordinates(pair.get(0), pair.get(1));
+        }
+
+        ArrayList<ArrayList<Integer>> ledPosPairs = coordinates.get("Led");
+        for (int i = 0; i < ledPosPairs.size(); i++) {
+            ArrayList<Integer> pair = ledPosPairs.get(i);
+            mainCircuit.getLeds().get(i).setCoordinates(pair.get(0), pair.get(1));
+        }
+
+        ArrayList<ArrayList<Integer>> gatesPosPairs = coordinates.get("LogicGate");
+        for (int i = 0; i < gatesPosPairs.size(); i++) {
+            ArrayList<Integer> pair = gatesPosPairs.get(i);
+            mainCircuit.getGates().get(i).setCoordinates(pair.get(0), pair.get(1));
+        }
+        mainCircuit.setIsMain(true);
+        return mainCircuit;
+    }
+>>>>>>> parent of cfcdb60... Merge remote-tracking branch 'origin/Cat' into Sam
 }
