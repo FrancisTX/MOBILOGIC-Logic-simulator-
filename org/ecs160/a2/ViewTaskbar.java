4bd783dec85da2990847543dcdb578f2bdc91657
cat-castle-99 <56051215+cat-castle-99@users.noreply.github.com>
2020-12-08 17:47:22 -0800

    Merging Sam

--- org/ecs160/a2/ViewTaskbar.java
+++ org/ecs160/a2/ViewTaskbar.java
@@ -1,5 +1,7 @@
 package org.ecs160.a2;
 
+import com.codename1.components.MultiButton;
+import com.codename1.io.Storage;
 import com.codename1.ui.layouts.GridLayout;
 
 import java.util.*;
@@ -14,6 +16,7 @@ import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.*;
 import com.codename1.ui.geom.Dimension;
 import com.codename1.ui.layouts.GridLayout;
+import org.ecs160.a2.Utilities.WidgetFactory;
 import org.ecs160.a2.Utilities.WorkspaceUtil;
 
 public class ViewTaskbar extends Container {
@@ -21,19 +24,27 @@ public class ViewTaskbar extends Container {
 
     private String[] buttonNames =
             {       "OR", "AND", "XOR", "SWI", "NOT",
-                    "NOR","NAND","XNOR", "LED", "DMY"};
+                    "NOR","NAND","XNOR", "LED", "SUB"};
+    private String[] WFnameCases =
+            {       "GateOR", "GateAND", "GateXOR", "Switch", "GateNOT",
+                    "GateNOR","GateNAND","GateXNOR", "Led", "Circuit"};
 
     private Hashtable<String, Button> buttons = new Hashtable<String, Button>();
 
     public ViewTaskbar(ViewWorkspace workspace) {
         super();
         this.workspace = workspace;
-
         this.setLayout(new GridLayout(2, 5));
+
         for (String buttonName : buttonNames) {
             Button button = new Button(buttonName);
             buttons.put(buttonName, button);
             this.add(button);
+//            if (buttonName != "LOAD"){
+//                button.addActionListener((evt) -> {
+//                    WorkspaceUtil.getInstance().setWidgetAddingStrategy(button.getText());
+//                });
+//            }
         }
 
         Button addANDButton = getButton("AND");
@@ -82,8 +93,29 @@ public class ViewTaskbar extends Container {
         });
 
 
+        Button loadSubCircuitButton = getButton("SUB");
+        String[] fileNames = Storage.getInstance().listEntries();
+        loadSubCircuitButton.addActionListener((evt) ->{
+            Dialog d = new Dialog();
+            d.setLayout(BoxLayout.y());
+            d.getContentPane().setScrollableY(true);
+            for(int i=0; i < fileNames.length; i++){
+                System.out.print(fileNames[i]);
+                Button mb = new Button(fileNames[i]);
+                d.add(mb);
+                mb.addActionListener(ee->{
+                    //loadSubCircuitButton.setText(mb.getText()); //Change button to name of button
+                    d.dispose();
+                    WidgetFactory.getInstance().setCircuitName(mb.getText());
+                    WorkspaceUtil.getInstance().setWidgetAddingStrategy("Circuit");
+                });
+            }
+            d.showPopupDialog(loadSubCircuitButton);
+        });
+
 
     }
 
     public Button getButton(String buttonName) { return buttons.get(buttonName);}
+
 }
